2e05cb723ca7adea2a5617f9ce4c509f2ea2e033
Chen Minqiang <ptpt52@gmail.com>
2017-12-29 16:57:14 +0800

    miwifi: refine default led status

--- target/linux/ramips/base-files/etc/diag.sh
+++ target/linux/ramips/base-files/etc/diag.sh
@@ -177,6 +177,8 @@ get_status_led() {
 	k2p|\
 	m3|\
 	mir3g|\
+	miwifi-r3|\
+	miwifi-mini|\
 	miwifi-nano)
 		status_led="$boardname:blue:status"
 		;;
@@ -193,8 +195,6 @@ get_status_led() {
 	m4-8M)
 		status_led="m4:blue:status"
 		;;
-	miwifi-r3|\
-	miwifi-mini|\
 	zte-q7)
 		status_led="$boardname:red:status"
 		;;
